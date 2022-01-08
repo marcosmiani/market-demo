02531347480d046082bb40a29947af27b52798b1
Marcos Agustin Miani <marcos.miani@gmail.com>
2022-01-08 14:09:46 +0100

    Add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import "./styles.css";
+import styled from "styled-components";
 import Header from "./Header";
+
+const Wrapper = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  text-align: center;
+`;
 
 export default function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <Header />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-    </div>
+    </Wrapper>
   );
 }
